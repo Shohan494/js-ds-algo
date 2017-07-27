0a7634138cbf021322d43b9b99ac2518b4a9693e
shohan494 <shohan12224943@gmail.com>
2017-07-27 17:08:31 +0600

    number conversion

--- javascript-datastructures-algorithms-second-edition/chapter03/04-DecimalToBinary.js
+++ javascript-datastructures-algorithms-second-edition/chapter03/04-DecimalToBinary.js
@@ -1,19 +1,19 @@
 //233 == 11101001
 //2x(10x10) + 3x(10) + 3x(1)
 
-function divideBy2(decNumber){
+function divideBy2(decNumber) {
 
     var remStack = new Stack(),
         rem,
         binaryString = '';
 
-    while (decNumber > 0){
+    while (decNumber > 0) {
         rem = Math.floor(decNumber % 2);
         remStack.push(rem);
         decNumber = Math.floor(decNumber / 2);
     }
 
-    while (!remStack.isEmpty()){
+    while (!remStack.isEmpty()) {
         binaryString += remStack.pop().toString();
     }
 
@@ -27,26 +27,74 @@ console.log(divideBy2(1000));
 /*
     The folow algorithm converts from base 10 to any base
  */
-function baseConverter(decNumber, base){
+function Stack() {
+
+    let items = [];
+
+    this.push = function(element) {
+        items.push(element);
+    };
+
+    this.pop = function() {
+        return items.pop();
+    };
+
+    this.peek = function() {
+        return items[items.length - 1];
+    };
+
+    this.isEmpty = function() {
+        return items.length == 0;
+    };
+
+    this.size = function() {
+        return items.length;
+    };
+
+    this.clear = function() {
+        items = [];
+    };
+
+    this.print = function() {
+        console.log(items.toString());
+    };
+
+    this.toString = function() {
+        return items.toString();
+    };
+}
+
+function baseConverter(decNumber, base) {
 
     var remStack = new Stack(),
         rem,
         baseString = '',
         digits = '0123456789ABCDEF';
 
-    while (decNumber > 0){
+    // continue to store the result each time in the array
+    while (decNumber > 0) {
+        console.log('decNumber', decNumber);
+        console.log('OPERATION : rem = Math.floor(' + decNumber + ' % ' + base + ')');
         rem = Math.floor(decNumber % base);
+        console.log('rem', rem);
+        console.log('OPERATION : remStack.push(rem)');
         remStack.push(rem);
+
+        console.log('OPERATION : decNumber: ' + decNumber + ' =' + ' Math.floor(' + decNumber + ' / ' + base + ')');
         decNumber = Math.floor(decNumber / base);
+
+        console.log('\n')
+
     }
 
-    while (!remStack.isEmpty()){
+    // continue to poping the result each time of the array
+    // and then adding the value to the baseString variable
+    while (!remStack.isEmpty()) {
         baseString += digits[remStack.pop()];
+        //console.log("pop", digits[remStack.pop()]);
+        console.log('baseString', baseString);
     }
-
     return baseString;
 }
 
-console.log(baseConverter(100345, 2));
-console.log(baseConverter(100345, 8));
-console.log(baseConverter(100345, 16));
+baseConverter(255, 16);
